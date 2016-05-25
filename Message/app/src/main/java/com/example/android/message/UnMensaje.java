69f5d365e320b913d5ef84a1c54243c0f83e325e
unknown <android@PC09WIN8.LINUX-DOMAIN>
2016-05-25 15:14:51 -0500

    CORRECCION

--- Message/app/src/main/java/com/example/android/message/UnMensaje.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.message;
-// Clase que representa un mensaje en el dispositivo,
-// contiene quien envió el mensaje y el mensaje que fue enviado
-
-public class UnMensaje {
-    int from;
-    String mensaje;
-}
--- Message/app/src/main/java/com/example/android/message/modelo/mensaje.java
+++
@@ -1,9 +0,0 @@
-package com.example.android.message.modelo;
-
-public class mensaje {
-    public int id;
-    public int from;
-    public int to;
-    public String text;
-    public String date;
-}
